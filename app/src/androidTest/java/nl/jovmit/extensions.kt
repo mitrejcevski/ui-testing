a2cc1719f213818aa40022050a9aac1cce043833
Ali Avci <aiavci@yahoo.com>
2018-09-26 10:58:02 -0400

    Fix extension methods. Convert them from ViewAssertions to ViewInteractions

--- app/src/androidTest/java/nl/jovmit/extensions.kt
+++ app/src/androidTest/java/nl/jovmit/extensions.kt
@@ -7,7 +7,11 @@ import android.support.test.espresso.ViewAssertion
 import android.support.test.espresso.ViewInteraction
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import android.support.test.espresso.matcher.ViewMatchers.withHint
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withParent
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.v7.widget.Toolbar
 import org.hamcrest.Matchers.allOf
 
@@ -20,14 +24,14 @@ fun text(@StringRes resource: Int): ViewInteraction = onView(withText(resource))
 
 infix fun ViewInteraction.check(action: ViewAssertion): ViewInteraction = this.check(action)
 
-infix fun ViewInteraction.hasHint(@StringRes string: Int): ViewAssertion = matches(withHint(string))
+infix fun ViewInteraction.hasHint(@StringRes string: Int): ViewInteraction = this check matches(withHint(string))
 
-infix fun ViewInteraction.hasText(@StringRes string: Int): ViewAssertion = matches(withText(string))
+infix fun ViewInteraction.hasText(@StringRes string: Int): ViewInteraction = this check matches(withText(string))
 
 infix fun Int.perform(action: ViewAction): ViewInteraction = onView(withId(this)).perform(action)
 
 infix fun Int.check(action: ViewAssertion): ViewInteraction = onView(withId(this)).check(action)
 
-infix fun Int.hasHint(@StringRes resource: Int): ViewAssertion = onView(withId(this)) hasHint resource
+infix fun Int.hasHint(@StringRes resource: Int): ViewInteraction = onView(withId(this)) hasHint resource
 
-infix fun Int.hasText(@StringRes resource: Int): ViewAssertion = onView(withId(this)) hasText resource
+infix fun Int.hasText(@StringRes resource: Int): ViewInteraction = onView(withId(this)) hasText resource
